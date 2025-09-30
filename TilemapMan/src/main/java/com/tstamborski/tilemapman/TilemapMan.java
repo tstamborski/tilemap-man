f4c200bbe6b189ac926dc98bafa4bb7719ba7538
tstamborski <tstamborski@outlook.com>
2025-09-30 17:02:23 +0200

    30.09.2025 upload empty maven project

---
+++ TilemapMan/src/main/java/com/tstamborski/tilemapman/TilemapMan.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tstamborski.tilemapman;
+
+/**
+ *
+ * @author Tobiasz Stamborski <tstamborski@outlook.com>
+ */
+public class TilemapMan {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
